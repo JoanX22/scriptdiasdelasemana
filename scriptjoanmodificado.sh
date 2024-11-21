6f57f72b4d5a8074354743dc5030270a20b7ea41
JoanX22 <jonieleven22@gmail.com>
2024-11-21 16:24:33 +0100

    scriptjoanmodificado.sh

---
+++ scriptjoanmodificado.sh
@@ -0,0 +1,26 @@
+#!/usr/env/bin bash
+mkdir  2025
+cd ./2025
+mkdir enero
+cd ./enero
+mkdir semana1
+cd ./semana1
+mkdir miercoles1 jueves2 viernes3 sabado4 domingo5
+cd ..
+mkdir semana2
+cd ./semana2
+mkdir lunes6 martes7 miercoles8 jueves9 viernes10 sabado11 domingo12
+cd ..
+mkdir semana3
+cd ./semana3
+mkdir lunes13 martes14 miercoles15 jueves16 viernes17 sabado18 domingo19
+cd ..
+mkdir semana4
+cd ./semana4
+mkdir lunes20 martes21 miercoles22 jueves23 viernes24 sabado25 domingo26
+cd ..
+mkdir semana5
+cd ./semana5
+mkdir lunes27 martes28 miercoles29 jueves30 viernes31
+
+
